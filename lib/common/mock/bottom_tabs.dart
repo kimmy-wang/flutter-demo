2e22b3fd33b707a00fd74fe5143f42e618bf8754
upcwangying <upcwangying@126.com>
2020-04-01 15:03:58 +0800

    refactor: 代码重构

---
+++ lib/common/mock/bottom_tabs.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_demo/ui/pages/samples/samples_page.dart';
+import 'package:flutter_demo/ui/pages/category/category_page.dart';
+import 'package:flutter_demo/ui/pages/webview/webview_page.dart';
+import 'package:flutter_demo/ui/pages/settings/settings_page.dart';
+
+List<Map<String, Object>> tabs = [
+  {
+    "title": "示例",
+    "icon": Icons.grid_on,
+    "widget": (title) =>
+        SamplesPage(
+          headerTitle: title,
+        )
+  },
+  {
+    "title": "分类",
+    "icon": Icons.format_list_numbered_rtl,
+    "widget": (title) =>
+        CategoryPage(
+          headerTitle: title,
+        )
+  },
+  {
+    "title": "网页",
+    "icon": Icons.open_in_browser,
+    "widget": (title) =>
+        WebviewPage(
+          headerTitle: title,
+        )
+  },
+  {
+    "title": "设置",
+    "icon": Icons.settings,
+    "widget": (title) =>
+        SettingsPage(
+          headerTitle: title,
+        )
+  }
+];
--- lib/ui/pages/tab_navigator.dart
+++ lib/ui/pages/tab_navigator.dart
@@ -1,46 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_demo/ui/pages/samples/samples_page.dart';
-import 'package:flutter_demo/ui/pages/category/category_page.dart';
-import 'package:flutter_demo/ui/pages/webview/webview_page.dart';
-import 'package:flutter_demo/ui/pages/settings/settings_page.dart';
 import 'package:flutter_demo/ui/store/state.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
-List<Map<String, Object>> tabs = [
-  {
-    "title": "示例",
-    "icon": Icons.grid_on,
-    "widget": (title) =>
-        SamplesPage(
-          headerTitle: title,
-        )
-  },
-  {
-    "title": "分类",
-    "icon": Icons.format_list_numbered_rtl,
-    "widget": (title) =>
-        CategoryPage(
-          headerTitle: title,
-        )
-  },
-  {
-    "title": "网页",
-    "icon": Icons.open_in_browser,
-    "widget": (title) =>
-        WebviewPage(
-          headerTitle: title,
-        )
-  },
-  {
-    "title": "设置",
-    "icon": Icons.settings,
-    "widget": (title) =>
-        SettingsPage(
-          headerTitle: title,
-        )
-  }
-];
+import 'package:flutter_demo/common/mock/bottom_tabs.dart';
 
 class TabNavigator extends StatefulWidget {
   @override
