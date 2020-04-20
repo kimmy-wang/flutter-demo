8f7ecfef1be15b0034f08af8f2dcc29509039dfd
upcwangying <upcwangying@126.com>
2020-04-20 16:31:51 +0800

    feat: nfc示例

--- lib/ui/pages/samples/nfc.dart
+++ lib/ui/pages/samples/nfc.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_demo/common/utils/navigator_util.dart';
+import 'package:flutter_demo/ui/pages/samples/nfc_read_screen.dart';
+import 'package:flutter_demo/ui/pages/samples/nfc_write_screen.dart';
+
 class Nfc extends StatefulWidget {
   final String headerTitle;
 
@@ -17,9 +21,25 @@ class _NfcState extends State<Nfc> {
       appBar: AppBar(
         title: Text(widget.headerTitle ?? "Nfc"),
       ),
-      body: Container(
-        child: Text("Nfc"),
-      ),
+      body: Builder(builder: (context) {
+        return ListView(
+          children: <Widget>[
+            ListTile(
+              title: const Text("Read NFC"),
+              onTap: () {
+                NavigatorUtil.push(context, NfcReadScreen());
+              },
+            ),
+            Divider(),
+            ListTile(
+              title: const Text("Write NFC"),
+              onTap: () {
+                NavigatorUtil.push(context, NfcWriteScreen());
+              },
+            ),
+          ],
+        );
+      }),
     );
   }
 
