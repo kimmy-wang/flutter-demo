dc99f3a2017796ca50e40803dfe4386b619d3553
upcwangying <upcwangying@126.com>
2020-04-24 09:11:17 +0800

    feat: 单例示例

---
+++ lib/common/flutter_demo_manager.dart
@@ -0,0 +1,27 @@
+class FlutterDemoManager {
+  String username;
+  String password;
+  int authorizationId;
+  String token;
+  String loginType;
+
+  // 单例公开访问点
+  factory FlutterDemoManager() => _flutterDemoManager();
+
+  // 静态私有成员，没有初始化
+  static FlutterDemoManager _instance;
+
+  // 私有构造函数
+  FlutterDemoManager._() {
+    // 具体初始化代码
+  }
+
+  // 静态、同步、私有访问点
+  static FlutterDemoManager _flutterDemoManager() {
+    if (_instance == null) {
+      _instance = FlutterDemoManager._();
+    }
+    return _instance;
+  }
+
+}
