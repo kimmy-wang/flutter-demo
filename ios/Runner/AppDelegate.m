70a7dde399f723b9d7616fbd20dbc62633e20045
upcwangying <upcwangying@126.com>
2020-05-03 09:41:44 +0800

    feat: add obj-c class

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import "FlutterDeveloper.h"
 
 @implementation AppDelegate
 
@@ -7,10 +8,13 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     int flutter_native_splash = 1;
     UIApplication.sharedApplication.statusBarHidden = false;
+    
+    FlutterDeveloper *developer = [[FlutterDeveloper alloc] initWith:@"wy" andAge:31];
+    [developer say:@"我不容易啊!"];
 
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    // Override point for customization after application launch.
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
